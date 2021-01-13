72c0dccac57375bb823d2623c0ae0af55203d289
Manel <manel@localhost.localdomain>
2021-01-13 19:28:38 +0100

    TS boilerplate working

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,23 +1,26 @@
-const webpack = require("webpack");
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-process.env.NODE_ENV = "development";
+process.env.NODE_ENV = 'development';
 
 module.exports = {
-  mode: "development",
-  target: "web",
-  devtool: "cheap-module-source-map",
-  entry: "./src/index",
+  target: 'web',
+  devtool: 'cheap-module-source-map',
+  entry: './src/Index.tsx',
   output: {
-    path: path.resolve(__dirname, "build"),
-    publicPath: "/",
-    filename: "bundle.js",
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
   },
   devServer: {
-    stats: "minimal",
+    stats: 'minimal',
     overlay: true,
     historyApiFallback: true,
+    contentBase: path.resolve(__dirname, './dist'),
   },
   plugins: [
     // saved for interesting posibilities
@@ -25,20 +28,21 @@ module.exports = {
     //     "process.env.API_URL": JSON.stringify("http://localhost:3001"),
     // }),
     new HtmlWebpackPlugin({
-      template: "src/index.html",
+      template: 'src/index.html',
       //favicon:"src/favicon.ico"
     }),
   ],
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
+        test: /\.(js|ts)x?$/,
         exclude: /node_modules/,
-        use: ["babel-loader", "eslint-loader"],
+        use: ['babel-loader'],
       },
       {
         test: /\.(css)$/,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
+        exclude: /node_modules/,
       },
     ],
   },
